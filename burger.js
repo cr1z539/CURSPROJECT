dc89f074dc9ff44fdf36500b5a6708a9101372a4
cr1z539 <minsafinvlad@gmail.com>
2025-01-28 14:48:09 +0300

    Delete burger.js

--- burger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const menu = document.querySelector('.list');
-    
-    burgerMenu.addEventListener('click', function() {
-      menu.classList.toggle('show');
-    });
-  });
